30cdf53e6e3521479d5948345fd4346b56b252e7
Sandra Rodgers <sandra.rodgers@deepgram.com>
2022-04-26 14:35:27 -0500

    starter files

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,6 +1,5 @@
 <template>
   <h1>To-Do List</h1>
-  <todo-speech />
   <todo-form />
   <todo-list />
 </template>
@@ -8,17 +7,13 @@
 <script>
 import TodoForm from "./TodoForm.vue";
 import TodoList from "./TodoList.vue";
-import TodoSpeech from "./TodoSpeech.vue";
 export default {
   components: {
     TodoForm,
     TodoList,
-    TodoSpeech,
   },
   setup() {
     return {};
   },
 };
 </script>
-
-<style scoped></style>
